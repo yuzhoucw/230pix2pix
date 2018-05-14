c7f7e6a336b818d2a3242a1f0b1ef376b7b5ff40
Weini <wnlscindy@gmail.com>
2018-05-14 13:35:23 -0700

    change data dir args

--- main.py
+++ main.py
@@ -35,12 +35,7 @@ parser.add_argument('--beta1', default=0.5, type=float, help='momentum term of a
 parser.add_argument('--lambd', default=100.0, type=float, help='weight for L1 loss')
 # Files
 parser.add_argument('--out_dir', default='./checkpoints', type=str)
-parser.add_argument('--train_A_dir', default='./datasets/maps/trainA', type=str)
-parser.add_argument('--train_B_dir', default='./datasets/maps/trainB', type=str)
-parser.add_argument('--val_A_dir', default='./datasets/maps/valA', type=str)
-parser.add_argument('--val_B_dir', default='./datasets/maps/valB', type=str)
-parser.add_argument('--test_A_dir', default='./datasets/maps/testA', type=str)
-parser.add_argument('--test_B_dir', default='./datasets/maps/testB', type=str)
+parser.add_argument('--data_dir', default='./datasets/maps/', type=str)
 
 # Visualization
 parser.add_argument('--vis', default=False, action='store_true')
@@ -75,9 +70,13 @@ if __name__ == "__main__":
         print("\nSave model and stats to directory %s" % (out_dir))
 
         # load data
-        train_loader = dataloader.get_dataloader(args.train_A_dir, args.train_B_dir, resize=args.resize, crop=args.crop,
+        train_loader = dataloader.get_dataloader(os.path.join(args.data_dir, "trainA"),
+                                                 os.path.join(args.data_dir, "trainB"),
+                                                 resize=args.resize, crop=args.crop,
                                                  batch_size=args.batch_size, unaligned=args.unaligned, device=device)
-        val_loader = dataloader.get_dataloader(args.val_A_dir, args.val_B_dir, resize=args.resize, crop=args.crop,
+        val_loader = dataloader.get_dataloader(os.path.join(args.data_dir, "valA"),
+                                               os.path.join(args.data_dir, "valB"),
+                                               resize=args.resize, crop=args.crop,
                                                batch_size=args.batch_size, unaligned=args.unaligned, device=device)
     if args.mode == "test":
         out_dir = os.path.dirname(args.pretrain_path)
@@ -85,7 +84,9 @@ if __name__ == "__main__":
         os.mkdir(out_dir_img)
 
         # load data
-        test_loader = dataloader.get_dataloader(args.test_A_dir, args.test_B_dir, resize=args.resize, crop=args.crop,
+        test_loader = dataloader.get_dataloader(os.path.join(args.data_dir, "testA"),
+                                                os.path.join(args.data_dir, "testB"),
+                                                resize=args.resize, crop=args.crop,
                                                 batch_size=args.batch_size, unaligned=args.unaligned, device=device)
 
     if args.vis:
