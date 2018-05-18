fbb21153b806b8f175012538429123fc448a058c
Weini <wnlscindy@gmail.com>
2018-05-18 01:34:57 -0700

    add device_id

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ parser.add_argument('--unaligned', default=False, type=bool)
 parser.add_argument('--resize', default=286, type=int)
 parser.add_argument('--crop', default=256, type=int)
 # Training
+parser.add_argument('--device_id', default=0, type=int)
 parser.add_argument('--mode', default="train", type=str)
 parser.add_argument('--pretrain_path', default='', type=str)
 parser.add_argument('--print_every_train', default=100, type=int)
@@ -45,8 +46,8 @@ parser.add_argument('--port', default=8097, type=int)
 
 if __name__ == "__main__":
 
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     args = parser.parse_args()
+    device = torch.device("cuda:%d" % args.device_id if torch.cuda.is_available() else "cpu")
 
     s = "Using %s\n\n" % device
     for k, v in vars(args).items():
