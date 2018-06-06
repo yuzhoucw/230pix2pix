d731efff26e054205eaa26da798d3370e92798d5
Weini <wnlscindy@gmail.com>
2018-06-06 01:46:38 -0700

    update d score plot

--- d_score.py
+++ d_score.py
@@ -3,10 +3,21 @@ import json
 
 with open("checkpoints/d_scores.json") as f:
     j = json.load(f)
-    plt.plot(j['scores_gen'], '.')
-    plt.title('D Scores gen')
-    plt.show()
+    fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(13,5))
 
-    plt.plot(j['scores_gt'], '.')
-    plt.title('D Scores gt')
-    plt.show()
+    x = j['scores_gen']
+    # x = sorted(j['scores_gen'], reverse=True)
+    ax1.plot(x, '.')
+    ax1.set_title('Train D Scores gen')
+    ax1.axhline(y=0.5, color='g')
+
+    x = j['scores_gt']
+    # x = sorted(j['scores_gt'], reverse=True)
+    ax2.plot(x, '.')
+    ax2.set_title('Train D Scores gt')
+    ax2.axhline(y=0.5, color='g')
+
+    fig.tight_layout()
+    # plt.show()
+
+    plt.savefig("checkpoints/d_scores_1.png")
